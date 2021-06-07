d3d5253aabf0d1031a87cccb363248644e052233
azza14 <42944667+azza14@users.noreply.github.com>
2021-06-07 15:27:59 +0200

    edit user controller

--- users/user.controller.js
+++ users/user.controller.js
@@ -18,16 +18,19 @@ module.exports=router;
             .then(user => user ? res.json(user): res.status (400).json({message: 'username or password is incorrect'}))
             .catch(err => next(err));
 }
+
 function register (req,res,next){
     userService.create(req.body)
            .then(() => res.json({}))
            .catch(err => next(err));
 }
+
 function getAll (req,res,next){
     userService.getAll(req.body)
            .then(users => res.json(users))
            .catch(err => next(err));
 }
+
 function getCurrent (req,res,next){
     userService.getById(req.user.sub)
            .then(user =>  user ? res.json(user): res.sendStatus(404))
